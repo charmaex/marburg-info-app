777a27f7bc0aca3d1b4d8b6be40b5dd1f552860b
Jan Dammshäuser <jandammshaeuser@me.com>
2016-02-07 20:33:20 +0100

    - Added content

---
+++ MarburgCityInfo/PhotosVC.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosVC.swift
+//  MarburgCityInfo
+//
+//  Created by Jan Dammshäuser on 07.02.16.
+//  Copyright © 2016 Jan Dammshäuser. All rights reserved.
+//
+
+import UIKit
+
+class PhotosVC: UIViewController {
+
+    @IBAction func backButton(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
---
+++ MarburgCityInfo/View.swift
@@ -0,0 +1,36 @@
+//
+//  ClipToBounds.swift
+//  MarburgCityInfo
+//
+//  Created by Jan Dammshäuser on 07.02.16.
+//  Copyright © 2016 Jan Dammshäuser. All rights reserved.
+//
+
+import UIKit
+
+class ClipToBoundsAndRoundEdges: UIImageView {
+    
+    override func awakeFromNib() {
+        layer.cornerRadius = 8
+        clipsToBounds = true
+    }
+    
+}
+
+class ClipToBounds: UIImageView {
+    
+    override func awakeFromNib() {
+        layer.cornerRadius = 8
+    }
+    
+}
+
+class RoundButton: UIButton {
+    
+    override func awakeFromNib() {
+        layer.cornerRadius = 3
+        backgroundColor = UIColor.orangeColor()
+        setTitleColor(UIColor.whiteColor(), forState: .Normal)
+    }
+    
+}
